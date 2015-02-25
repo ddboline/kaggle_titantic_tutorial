6abd310f306ddab6af2ca11b087d1deef0b6f92d
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-25 16:44:40 -0500

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+rm ~/.ssh/known_hosts
+scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/kaggle_titantic_tutorial/*.csv .
+
 ./mymodel.py
+
+ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk DONE_TITANTIC"
